9fc4c0ff19ee6582056fb8dda1c2891c5a9284a7
Jesper Jensen <jesper.jensen@keemail.me>
2023-09-19 14:05:02 +0200

    feat: Added excercise 5 adn updated 4

--- modules-and-named-fuctions/exercises/modules-and-functions-1.exs
+++ modules-and-named-fuctions/exercises/modules-and-functions-1.exs
@@ -1,5 +1,12 @@
+#This exersice covers implementation from 1 - 3
 defmodule Times do
   def double(n), do: n * 2
   def triple(n), do: n * 3
   def quardruple(n), do: double(double(n))
 end
+
+# 2: compile code
+# iex c "exercises/modules-and-functions-1.exs"
+# in iex: run
+# Times.double(4), returns 8
+# Times.triple(4), returns 12
---
+++ modules-and-named-fuctions/exercises/modules-and-functions-4.exs
@@ -0,0 +1,4 @@
+# Implement function and run sum(n)
+# in iex run: c "../recursive.exs"
+#  Recursive.sum(4), result 10
+#  Recursive.sum(5), result 15
